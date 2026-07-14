0668b3369843d26603bf2348b8836c0abb06acfb
matikoxfirefox <ffox77343@gmail.com>
2026-07-14 13:58:57 +0200

    Added: - User Register completed (waiting for backend) - Axios - Success/Error message for user

--- src/Components/RegisterScreen.jsx
+++ src/Components/RegisterScreen.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import axios from 'axios';
 export default function RegisterScreen() {
+
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
