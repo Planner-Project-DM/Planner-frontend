023531b64de7a6aef8581ad5e0b57b0fbdaf1225
matikoxfirefox <ffox77343@gmail.com>
2026-07-14 11:13:18 +0200

    Fix: - Delted unnecessary ID's from HTML tag's

--- src/Components/RegisterScreen.jsx
+++ src/Components/RegisterScreen.jsx
@@ -4,18 +4,23 @@ export default function RegisterScreen() {
 
     return (
         <div className={"flex flex-row font-playpen"}>
-            <div id="registerSide" className={"bg-[url('/loginScreen.png')] bg-cover w-full h-screen"}>
-                <div id="registerCover" className="bg-primary backdrop-blur-sm  bg-black/60 h-full">
-                    <div id="aboutSection" className={"flex p-12 w-full"}>
-                        <div id="registerAbout" className={" w-full"}>
-                            <div id="registerTitle" className={"text-white mt-12 font-semibold "}>PLANER PODRÓŻY - REJESTRACJA</div>
-                            <div id="registerText" className={"text-white font-bold text-8xl mt-5 "}>Zaplanuj <br/> <i className={"text-yellow-500 m-5"}>podróż</i> życia</div>
-                            <div id="registerDesc"><p className={"text-white font-semibold text-xl mt-10"}>Twórz trasy, śledź rezerwacje i odkrywaj <br/>
-                            miejsca, o których jeszcze nie wiesz, że <br/>
+            <div className={"bg-[url('/loginScreen.png')] bg-cover w-full h-screen"}>
+                <div className="bg-primary backdrop-blur-sm  bg-black/60 h-full">
+                    <div className={"flex p-12 w-full"}>
+                        <div className={" w-full"}>
+                            <div className={"text-white mt-12 font-semibold "}>PLANER PODRÓŻY -
+                                REJESTRACJA
+                            </div>
+                            <div className={"text-white font-bold text-8xl mt-5 "}>Zaplanuj <br/> <i
+                                className={"text-yellow-500 m-5"}>podróż</i> życia
+                            </div>
+                            <div><p className={"text-white font-semibold text-xl mt-10"}>Twórz trasy,
+                                śledź rezerwacje i odkrywaj <br/>
+                                miejsca, o których jeszcze nie wiesz, że <br/>
                                 chcesz odwiedzić.</p>
                             </div>
                         </div>
-                        <div id="regLogPhoto" className={"w-1/2 flex items-center justify-center"}>
+                        <div className={"w-1/2 flex items-center justify-center"}>
                             <img src="/reg_logPhoto.png" alt="Vacation Photo" className={"w-72 mt-5"}/>
                         </div>
                     </div>
@@ -29,39 +34,42 @@ export default function RegisterScreen() {
                     </div>
                 </div>
             </div>
-            <div id={'registerMain'} className="bg-bg-main w-1/2 h-screen">
-                    <div id='registerCard' className="flex flex-col p-8 bg-cyan-900 w-full h-full">
-                        <p className="flex text-3xl font-bold text-[#FFFFFF] flex-col items-center justify-center">
-                            Rejestracja
-                        </p>
-                        <form id="registerForm" className={"flex flex-col text-white gap-5"}>
-                            <div id="registerUInfo" className={"flex gap-5"}>
-                                <input type="text" id="registerFName" placeholder="Imię" className={"w-1/2"}/>
-                                <input type="text" id="registerLName" placeholder="Nazwisko" className={"w-1/2"}/>
-                            </div>
-                            <input type="text" id="registerMail" placeholder="Adres E-mail"/>
-                            <input type="password" id="registerPswd" placeholder="Hasło (Minimum 8 znaków)"/>
-                            <div id="registerQSection" className={"flex flex-col mt-10"}>
-                                <div id="remember" className={"flex items-center gap-2"}>
-                                    <div id="rememberCheck" className={"flex items-center"}>
-                                        <input type="checkbox" id="registerRemember"/>
-                                    </div>
-                                    <label htmlFor="registerRemember">Zapamiętaj mnie</label>
+            <div className="bg-bg-main w-1/2 h-screen">
+                <div className="flex flex-col p-8 bg-cyan-900 w-full h-full">
+                    <p className="flex text-3xl font-bold text-[#FFFFFF] flex-col items-center justify-center">
+                        Rejestracja
+                    </p>
+                    <form id="registerForm" className={"flex flex-col text-white gap-5"}>
+                        <div className={"flex gap-5"}>
+                            <input type="text" id="registerFName" placeholder="Imię" className={"w-1/2"}/>
+                            <input type="text" id="registerLName" placeholder="Nazwisko" className={"w-1/2"}/>
+                        </div>
+                        <input type="text" id="registerMail" placeholder="Adres E-mail"/>
+                        <input type="password" id="registerPswd" placeholder="Hasło (Minimum 8 znaków)"/>
+                        <div className={"flex flex-col mt-10"}>
+                            <div id="remember" className={"flex items-center gap-2"}>
+                                <div className={"flex items-center"}>
+                                    <input type="checkbox" id="registerRemember"/>
                                 </div>
-                                <button type={"submit"} className={"bg-accent hover:bg-accent-hover b rounded-2xl h-12 font-bold text-xl"}>Zarejestruj</button>
+                                <label htmlFor="registerRemember">Zapamiętaj mnie</label>
                             </div>
-                        </form>
-                        <hr className={"mt-12 border-2 rounded-2xl"}/>
-                        <div id="registerFooter" className={"flex items-center justify-center flex-col gap-5 mt-12"}>
-                            <button id="registerGoogle" className={"bg-white hover:bg-gray-300 min-h-12 w-full rounded-2xl flex items-center justify-center gap-3"}>
-                                <img src="/LogoGoogleIcon.avif" alt="Google Logo" className={"h-10"}/>
-                                <p className={"font-semibold"}>Kontynuuj przez konto Google</p>
+                            <button type={"submit"}
+                                    className={"bg-accent hover:bg-accent-hover b rounded-2xl h-12 font-bold text-xl"}>Zarejestruj
                             </button>
                         </div>
-                            <button id="registerAccQ" className={"text-white mt-20 font-bold flex items-center justify-center"}>Masz już konto?
-                                <p className={"ml-2 text-accent"}>Zaloguj się</p>
-                            </button>
+                    </form>
+                    <hr className={"mt-12 border-2 rounded-2xl"}/>
+                    <div className={"flex items-center justify-center flex-col gap-5 mt-12"}>
+                        <button
+                            className={"bg-white hover:bg-gray-300 min-h-12 w-full rounded-2xl flex items-center justify-center gap-3"}>
+                            <img src="/LogoGoogleIcon.avif" alt="Google Logo" className={"h-10"}/>
+                            <p className={"font-semibold"}>Kontynuuj przez konto Google</p>
+                        </button>
                     </div>
+                    <button className={"text-white mt-20 font-bold flex items-center justify-center"}>Masz już konto?
+                        <p className={"ml-2 text-accent"}>Zaloguj się</p>
+                    </button>
+                </div>
             </div>
         </div>
     );
