1530cc8689f9fea07c85df827cd11ac15b5c892a
matikoxfirefox <ffox77343@gmail.com>
2026-07-18 13:36:43 +0200

    Added: - Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Navbar(){
+    return(
+        <main className={"flex"}>
+            <div>
+                Nazwa podróży
+            </div>
+            <div>
+                Wyszukiwarka
+            </div>
+            <div>
+                Aktwyne/Zakonczone podróże
+            </div>
+        </main>
+    );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,17 @@
+import Navbar from "../components/Navbar.jsx";
+
+
 export default function Dashboard(){
     return (
-        <div className={"h-screen w-screen bg-bg-main"}>
-
-        </div>
+        <>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <aside></aside>
+                <div id="mainWindow" ></div>
+                <aside></aside>
+            </main>
+        </>
     )
 }
