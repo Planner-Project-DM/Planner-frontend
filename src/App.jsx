fbc10d1bd596868e8f50cb06094dfc68e7b9223e
matikoxfirefox <ffox77343@gmail.com>
2026-07-18 13:11:47 +0200

    Added: - Navigating user to login after token expires

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from "./pages/loginScreen.jsx";
 import RegisterScreen from "./pages/RegisterScreen.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+function isTokenValid() {
+    const token = localStorage.getItem('userToken') || sessionStorage.getItem('userToken');
+    if (!token || token === "undefined") return false;
+
+    try {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        if (payload.exp * 1000 > Date.now()){
+            return true;
+        } else {
+            localStorage.clear();
+            sessionStorage.clear();
+            return false;
+        }
+    } catch {
+        return false;
+    }
+}
+
 
 function App() {
-    const token = localStorage.getItem('userToken') || sessionStorage.getItem('userToken');
-    const [isLoggedIn, setIsLoggedIn] = useState(token !== null && token !== "undefined");
+    const [isLoggedIn, setIsLoggedIn] = useState(isTokenValid);
 
     return (
         <BrowserRouter>
