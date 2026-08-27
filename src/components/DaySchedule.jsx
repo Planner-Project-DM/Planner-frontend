acccc5bf66bd753dc5021e41568f8235d02b0883
matikoxfirefox <ffox77343@gmail.com>
2026-08-27 19:16:49 +0200

    Added: - Calendar import for website.
    To do:
    Combine calendar component with webstie tripitems
    
    To fix:
    Month view

--- src/components/DaySchedule.jsx
+++ src/components/DaySchedule.jsx
@@ -1,80 +1,36 @@
-import {useState, useRef} from 'react';
-import FilterButtons from '../components/FilterButtons.jsx'
+import {useState} from 'react';
+import { Calendar, Editor, Willow } from "@svar-ui/react-calendar";
+import "@svar-ui/react-calendar/all.css"
 
 export default function DaySchedule({activeTrip, schedules}) {
-    const [selectedDay, setSelectedDay] = useState(null);
-    const scrollRef = useRef(null)
-    const [isDragging, setIsDragging] = useState(false);
-    const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
-    const [wasDragged, setWasDragged] = useState(false);
-    // if (schedules === null) {
-    //     return;
-    // } else {
-    //     const events = [{
-    //         id: schedules.id,
-    //         tripItem: {
-    //             id: schedules.tripItem.id,
-    //             name: schedules.tripItem.name,
-    //         },
-    //         startTime: schedules.startTime,
-    //         endTime: schedules.endTime,
-    //     }]
-    // }
+    const [api, setApi] = useState(null);
+
+    let events = []
+
+    if (schedules === null) {
+        return;
+    } else {
+       events = schedules.map((map) => (
+           {
+               id: map.id,
+               start: new Date(map.startTime),
+               end: new Date(map.endTime),
+               text: map.tripItem.name,
+           }
+       ))
+    }
     if (activeTrip === null) {
         return (<div className={"w-full h-full flex items-center justify-center text-5xl"}>
             Wybierz podróż!
         </div>);
     }
-
-    const start = new Date(activeTrip.startDate);
-    const end = new Date(activeTrip.endDate);
-    const date = [];
-
-    while (start <= end) {
-        date.push(new Date(start));
-        start.setDate(start.getDate() + 1)
-    }
     return (
         <div className={"w-full h-full overflow-hidden p-3"}>
-            <div className={"overflow-y-scroll flex flex-col justify-center [&::-webkit-scrollbar]:hidden"}>
-                <div className={"flex gap-3 w-full overflow-x-auto p-3 [&::-webkit-scrollbar]:hidden"}
-                     ref={scrollRef}
-                     onMouseDown={(e) => {
-                         setWasDragged(false)
-                         setIsDragging(true);
-                         setStartX(e.pageX);
-                         setScrollLeft(scrollRef.current.scrollLeft);
-                     }}
-                     onMouseMove={(e) => {
-                         if(!isDragging){
-                             return null;
-                         } else {
-                             const diff = e.pageX - startX;
-                             Math.abs(diff)  > 5 ? setWasDragged(true) : setWasDragged(false);
-                             scrollRef.current.scrollLeft = scrollLeft - diff;
-                         }
-                     }}
-                     onMouseUp={() => setIsDragging(false)}
-                     onMouseLeave={() => setIsDragging(false)}
-                >
-
-                    {date.map((day) => (
-                        <FilterButtons
-                            onclick={() => {
-                                if (wasDragged) return;
-                                setSelectedDay(day)
-                            }}
-                            isActive={selectedDay?.getTime() === day.getTime()}
-                            name={day.toLocaleDateString('pl-PL', {weekday: 'short'}) + " " + day.toLocaleDateString('pl-PL', {
-                                day: '2-digit',
-                                month: '2-digit'
-                            })}
-                            padding={"pl-2 pr-2"}
-                        />
-                    ))}
-                </div>
-                <div></div>
+            <div className={"h-full w-full overflow-y-auto"}>
+                <Willow>
+                    <Calendar init={setApi} events={events} date={new Date(activeTrip.startDate)} view={"month"} />
+                    {api && <Editor api={api} />}
+                </Willow>
             </div>
         </div>
     )
